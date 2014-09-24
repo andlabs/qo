b08453c2f5432b16c17f1ed3a48d6e73a7d504ad
Pietro Gagliardi <pietro10@mac.com>
2014-09-24 19:41:37 -0400

    Added the basic flag compiler.

---
+++ flagcompiler.go
@@ -0,0 +1,12 @@
+// 24 september 2014
+
+package main
+
+var toolchain Toolchain
+
+func compleFlags() {
+	// copy the initial values
+	toolchain = *selectedToolchain
+
+	// TODO read each file and append flags
+}
