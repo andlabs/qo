c6f685007d70c6684b439e9fa4580ed854dc4324
Pietro Gagliardi <pietro10@mac.com>
2014-09-25 14:08:45 -0400

    Added the #qo directive.

--- flagcompiler.go
+++ flagcompiler.go
@@ -6,11 +6,48 @@ import (
 	"flag"
 	"os"
 	"strings"
+	"bufio"
 )
 
 var debug = flag.Bool("g", false, "build with debug symbols")
 
 var toolchain Toolchain
+
+func parseFile(filename string) {
+	f, err := os.Open(filename)
+	if err != nil {
+		// TODO
+		panic(err)
+	}
+	defer f.Close()
+	r := bufio.NewScanner(f)
+
+	for r.Scan() {
+		line := r.Text()
+		if !strings.HasPrefix(line, "// #qo ") {
+			continue
+		}
+		line = line[len("// #qo "):]
+		parts := strings.Fields(line)
+		switch parts[0] {
+		case "CFLAGS:":
+			toolchain.CFLAGS = append(toolchain.CFLAGS, parts[1:]...)
+		case "CXXFLAGS:":
+			toolchain.CXXFLAGS = append(toolchain.CXXFLAGS, parts[1:]...)
+		case "LDFLAGS:":
+			toolchain.LDFLAGS = append(toolchain.LDFLAGS, parts[1:]...)
+		case "pkg-config:":
+			// TODO
+		default:
+			// TODO
+			panic("invalid line")
+		}
+	}
+	if err := r.Err(); err != nil {
+		// TODO
+		panic(err)
+	}
+}
 
 func compileFlags() {
 	if *selectedToolchain == "" {
@@ -33,5 +70,12 @@ func compileFlags() {
 		toolchain.CFLAGS = append(toolchain.CFLAGS, toolchain.CDEBUG...)
 		toolchain.CXXFLAGS = append(toolchain.CXXFLAGS, toolchain.CDEBUG...)
 		toolchain.LDFLAGS = append(toolchain.LDFLAGS, toolchain.LDDEBUG...)
+	}
+
+	for _, f := range cfiles {
+		parseFile(f)
+	}
+	for _, f := range cppfiles {
+		parseFile(f)
 	}
 }
