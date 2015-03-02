79b0457105951b58a7f09a3181226c2ccc3cbf75
Pietro Gagliardi <pietro10@mac.com>
2015-03-02 02:52:14 -0500

    Rewrote the builder to be load-balanced. It will now make no more goroutines than runtime.NumCPU(); this will be made an option later.

--- builder.go
+++ builder.go
@@ -5,9 +5,9 @@ package main
 import (
 	"fmt"
 	"os"
+	"runtime"
+	"sync"
 )
-
-var builder = make(chan *Executor)
 
 var percentPer float64
 var progress float64
@@ -16,30 +16,50 @@ func printProgress(step string) {
 	fmt.Printf("[%3d%%] %s\n", int(progress), step)
 }
 
-func runStage(s Stage) {
-	indices := make(map[*Executor]int)
-	for i, e := range s {
-		indices[e] = i
-		go e.Do()
+func runner(in <-chan *Step, out chan<- *Step, wg *sync.WaitGroup) {
+	for s := range in {
+		s.Do()
+		out <- s
 	}
-	for len(indices) != 0 {
-		e := <-builder
-		delete(indices, e)
-		fmt.Fprintf(os.Stderr, "%s", e.Output.Bytes())
+	wg.Done()
+}
+
+func runStage(s Stage) {
+	wg := new(sync.WaitGroup)
+	in := make(chan *Step)
+	out := make(chan *Step)
+	// TODO make an option
+	for i := 0; i < runtime.NumCPU(); i++ {
+		wg.Add(1)
+		go worker(in, out, wg)
+	}
+	wg.Add(1)
+	go func() {
+		for i := 0; i < len(s); i++ {
+			in <- s[i]
+		}
+		close(in)
+		wg.Done()
+	}()
+	for i := 0; i < len(s); i++ {
+		s := <-out
+		fmt.Fprintf(os.Stderr, "%s", s.Output.Bytes())
 		// ensure only one newline
-		if e.Output.Len() != 0 && e.Output.Bytes()[e.Output.Len() - 1] != '\n' {
+		if s.Output.Len() != 0 && s.Output.Bytes()[s.Output.Len() - 1] != '\n' {
 			fmt.Fprintf(os.Stderr, "\n")
 		}
-		if e.Error != nil {
-			fail("Step %q failed with error: %v", e.Name, e.Error)
+		if s.Error != nil {
+			fail("Step %q failed with error: %v", s.Name, s.Error)
 		}
 		progress += percentPer
-		printProgress(e.Name)
+		printProgress(s.Name)
 	}
+	wg.Wait()
+	close(out)
 }
 
 func run() {
-	percentPer = 100 / float64(nStages)
+	percentPer = 100 / float64(nSteps)
 	progress = 0
 	printProgress("Beginning build")
 	for _, stage := range script {
