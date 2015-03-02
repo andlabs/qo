8ba8f7bc36be59c0c9a8f06d1434b04deebd2035
Pietro Gagliardi <pietro10@mac.com>
2015-03-02 02:30:41 -0500

    Renamed Executor to Step. The whole terminology is going to be reworked a bit.

---
+++ step.go
@@ -0,0 +1,34 @@
+// 24 september 2014
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"flag"
+	"bytes"
+	"strings"
+)
+
+var showall = flag.Bool("x", false, "show all commands as they run")
+
+// Step represents a single step in the build process.
+type Step struct {
+	Name	string
+	Line		[]string
+	Output	*bytes.Buffer
+	Error		error
+}
+
+func (s *Step) Do() {
+	if *showall {
+		fmt.Printf("%s\n", strings.Join(s.Line, " "))
+	}
+	cmd := exec.Command(s.Line[0], s.Line[1:]...)
+	cmd.Env = os.Environ()
+	s.Output = new(bytes.Buffer)
+	cmd.Stdout = s.Output
+	cmd.Stderr = s.Output
+	s.Error = cmd.Run()
+}
